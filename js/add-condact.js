905db2c0047f3f4d558071ef662550ad8c5110ff
duaaismail24 <doaaee7@gmail.com>
2019-11-06 11:17:22 +0200

    lab06

---
+++ js/add-condact.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var greeting;
+var thename;
+thename = prompt("whats your name");
+
+if (thename = "Duaa") {
+greeting = "welcome Duaa";
+} else {
+greeting = "go out!!";
+}
+
+document.write(greeting);
+
+
