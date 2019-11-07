835f4da4c491da95c9a1003c6680669b7c73015b
duaaismail24 <doaaee7@gmail.com>
2019-11-07 10:42:14 +0200

    creating web

--- js/add-condact.js
+++ js/add-condact.js
@@ -1,4 +1,5 @@
 'use strict';
+alert("write your name");
 var complimenting = function(){
 var language;
 language = prompt("Do you like French ? 0-10");
@@ -11,8 +12,8 @@ compliment = "You Are welcome";
 } else {
 compliment = "go out!!";
 }
-
 }
+
 function WriteLevl(){
    var frenchLevel;
    frenchLevel = prompt('what is your level in French? 0-10');
@@ -23,14 +24,41 @@ function WriteLevl(){
     if (frenchLevel > 5 && frenchLevel<=10  ) {
       compliment = 'Hello smart!';
     } else if (frenchLevel > 5 && frenchLevel<=5) {
-      compliment = 'Good afternoon!';
+      compliment = 'Hello Friend!';
     } else if (frenchLevel =0) {
       compliment = 'comme on!';
     } else {
-      compliment = 'Please eneter a valid number';
+      compliment = 'come on!!';
     }
   
     document.write('<h3>' + compliment + ' ' + name + '</h3>');
   } 
 
 WriteLevl();
+
+// function compliment(){
+// Ask the user for the language level
+var level = prompt("beginner or fluent?");
+while(level != "beginner" && level != "fluent"){
+  level = prompt("wrong answer! beginner or fluent?");
+}
+
+// Get the picture source
+var imageSource;
+if (level === "beginner"){
+  imageSource = 'https://www.thoughtco.com/learn-french-french-for-beginners-1369365';
+}else if (level==="fluent"){
+  imageSource = 'images/hotel.png';
+}
+// ask the user for number of courses
+var numOfICourses = prompt("How much courses you want to get?");
+// Validate the number of courses
+while(numOfICourses=='' || isNaN(numOfICourses)){
+  numOfICourses = prompt("Wrong answer! How mauch course you want to get?");
+}
+// Build the <img> element to add it to the page
+var imgElement = '<img src="'+imageSource+'"/>';
+// make a loop to repeat the courses
+for (var i=0; i<numOfICourses; i+=1){
+  document.write(imgElement)
+}
